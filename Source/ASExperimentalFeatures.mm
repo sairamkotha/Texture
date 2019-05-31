ee31272de5a20f9ecbe496cffa449e551aa1f6f7
Adlai Holler <adlai@google.com>
2019-05-30 21:09:54 -0700

    Fix experiment mapping

--- Source/ASExperimentalFeatures.mm
+++ Source/ASExperimentalFeatures.mm
@@ -25,7 +25,8 @@ NSArray<NSString *> *ASExperimentalFeaturesGetNames(ASExperimentalFeatures flags
                                       @"exp_oom_bg_dealloc_disable",
                                       @"exp_transaction_operation_retain_cycle",
                                       @"exp_remove_textkit_initialising_lock",
-                                      @"exp_drawing_global"]));
+                                      @"exp_drawing_global",
+                                      @"exp_node_context"]));
   if (flags == ASExperimentalFeatureAll) {
     return allNames;
   }
